4feff1b13ec16e00ce23fb6166d70eeffcb39d4d
EkinKar <ekin1260@hotmail.com>
2022-12-29 00:53:16 +0300

    blog fix

--- app/src/main/java/com/example/dietapp/Blogs.java
+++ app/src/main/java/com/example/dietapp/Blogs.java
@@ -1,11 +1,17 @@
 package com.example.dietapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Blogs extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.blogs);
+    }
+    public void goBlog1(View v){
+        startActivity(new Intent(Blogs.this, Blog1.class));
+
     }
 }
