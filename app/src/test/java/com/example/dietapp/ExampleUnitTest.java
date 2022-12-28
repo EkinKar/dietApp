9805c1c8e8e0fa1d29721ff717035a857af301f4
erdemturk <mertbeyaz@posta.mu.edu.tr>
2022-12-28 22:08:48 +0300

    ui fixed

--- app/src/test/java/com/example/dietapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/dietapp/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.dietapp;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,10 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private CalculateBmi calculateBmi;
+
+    @Before
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
