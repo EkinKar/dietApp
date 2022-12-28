6bb3571693989bdd9fb3672a5b9e58f935c4b7d6
erdemturk <mertbeyaz@posta.mu.edu.tr>
2022-12-29 00:57:35 +0300

    added blogs pages

---
+++ app/src/main/java/com/example/dietapp/Blog1.java
@@ -0,0 +1,50 @@
+package com.example.dietapp;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class Blog1 extends Activity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blog1);
+
+        imageView = findViewById(R.id.blog1Image);
+
+        // Create and start the background thread
+        Thread thread = new Thread(new DownloadImageRunnable());
+        thread.start();
+    }
+
+    private class DownloadImageRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            // Download the image
+            try {
+                URL url = new URL("https://diettogo.com/data/fe/image/good-night-sleep_HEADER.jpg");
+                InputStream inputStream = url.openStream();
+                final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                // Update the ImageView on the main thread
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageView.setImageBitmap(bitmap);
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/dietapp/Blogs.java
+++ app/src/main/java/com/example/dietapp/Blogs.java
@@ -1,11 +1,27 @@
 package com.example.dietapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Blogs extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.blogs);
     }
+
+    public void getBlog1Page(View v){
+        startActivity(new Intent(Blogs.this, Blog1.class));
+
+    }
+//    public void getBlog2Page(View v){
+//        startActivity(new Intent(Blogs.this, Blog2.class));
+//    }
+//    public void getBlog3Page(View v){
+//        startActivity(new Intent(Blogs.this, Blog3.class));
+//    }
+//    public void getBlog4Page(View v){
+//        startActivity(new Intent(Blogs.this, Blog4.class));
+//    }
 }
